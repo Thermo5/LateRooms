11f2bba105b77cdd7fc55557e0ee1cdd4653373d
Stephen McKeown <stephenmckeown@Stephens-MacBook-Pro.local>
2018-02-26 16:48:52 +0000

    radio buttons added for filters

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   state = {
     loading: true,
     hotels: [],
-    carpark: true,
+    parking: true,
     gym: true,
     pool: true,
   }
@@ -26,26 +26,46 @@ class App extends Component {
       <div className="App">
         <Title />
 
-        {loading ? <p>Loading...</p> :
-          hotels.map((hotel, i) =>
-            <div className="section" key={i}>
-              <div className="box">
-                <article className="media">
-                  <div className="media-content">
-                    <div className="content">
-                      <h2><strong>{hotel.Name}</strong></h2>
-                      <h3>Star Rating: {hotel.StarRating}</h3>
-                      <h4>Facilities:</h4>
-                      {hotel.Facilities.map((facilite, i) =>
-                        <ul key={i}>{facilite}</ul>
-                      )}
-                    </div>
+        <div className="tile is-ancestor">
+          <div className="tile is-vertical is-10">
+
+            {loading ? <p>Loading...</p> :
+              hotels.map((hotel, i) =>
+                <div className="section" key={i}>
+                  <div className="box">
+                    <article className="media">
+                      <div className="media-content">
+                        <div className="content">
+                          <h2><strong>{hotel.Name}</strong></h2>
+                          <h3>Star Rating: {hotel.StarRating}</h3>
+                          <h4>Facilities:</h4>
+                          {hotel.Facilities.map((facilite, i) =>
+                            <ul key={i}>{facilite}</ul>
+                          )}
+                        </div>
+                      </div>
+                    </article>
                   </div>
-                </article>
-              </div>
+                </div>
+              )
+            }
+
+          </div>
+          <div className="tile is-vertical">
+            <div className="section">
+              <h3>Filters</h3>
+              <ul>
+                <li> <label class="radio"><input type="radio" name="parking" />Parking</label></li>
+                <li><label class="radio"><input type="radio" name="gym" />Gym</label></li>
+                <li><label class="radio"><input type="radio" name="pool" />Pool</label></li>
+              </ul>
             </div>
-          )
-        }
+          </div>
+        </div>
+
+
+
+
       </div>
     );
   }
