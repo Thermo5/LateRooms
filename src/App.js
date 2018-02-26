b54b8429a421777eb53e9e6b51612887fc0f0e44
Stephen McKeown <stephenmckeown@Stephens-MacBook-Pro.local>
2018-02-26 14:57:43 +0000

    Initial commit, title page rendered

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,25 @@
+
+
+import React, { Component } from 'react';
+
+
+class Title extends Component {
+
+  render() {
+    return (
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">LateRooms</h1>
+            <h2 class="subtitle">
+              Hotels
+          </h2>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+
+export default Title;
